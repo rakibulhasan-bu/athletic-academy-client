61fa78d4775eeb99a4621d570554fc03c5de7ce8
Rakibul Hasan <rakibulhasanbu@gmail.com>
2023-06-10 15:45:13 +0600

    use instrutor and use student system applied

---
+++ src/hooks/useStudent.jsx
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { useContext } from "react";
+import { AuthContext } from "../provider/AuthProvider";
+import useAxiosSecure from "./useAxiosSecure";
+
+const useStudent = () => {
+  const { user, loading } = useContext(AuthContext);
+  const [axiosSecure] = useAxiosSecure();
+
+  const { data: isStudent, isLoading: isStudentLoading } = useQuery({
+    queryKey: ["isStudent", user?.email],
+    enabled: !loading,
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/users/student/${user?.email}`);
+      return res.data.student;
+    },
+  });
+  return [isStudent, isStudentLoading];
+};
+export default useStudent;
--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -16,8 +16,12 @@ import {
 import { useContext } from "react";
 import { AuthContext } from "../provider/AuthProvider";
 import useAdmin from "../hooks/useAdmin";
+import useInstructor from "../hooks/useInstructor";
+import useStudent from "../hooks/useStudent";
 
 const DashboardLayout = () => {
+  const [isStudent] = useStudent();
+  const [isInstructor] = useInstructor();
   const [isAdmin] = useAdmin();
   const { logOut } = useContext(AuthContext);
   const handleLogOut = () => {
@@ -27,9 +31,71 @@ const DashboardLayout = () => {
   };
   return (
     <div className="flex">
-      <div className="h-screen w-1/5 overflow-y-auto bg-gray-50 px-6 py-10 text-lg dark:bg-gray-800">
+      <div className="h-screen w-1/5 overflow-y-auto bg-gray-50 px-6 py-20 text-lg dark:bg-gray-800">
         {/* these is links based on user admin and instractor  */}
         <ul className="space-y-2 font-medium">
+          {isStudent && (
+            <>
+              <li>
+                <NavLink
+                  to="/dashboard/selectedClasses"
+                  className="flex items-center rounded-lg p-2 text-gray-600 hover:bg-gray-200 hover:text-primary"
+                >
+                  <MdOutlineClass className="text-xl font-bold" />
+                  <span className="ml-3 flex-1 whitespace-nowrap">
+                    Selected Classes
+                  </span>
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="/dashboard/enrolledClasses"
+                  className="flex items-center rounded-lg p-2 text-gray-600 hover:bg-gray-200 hover:text-primary"
+                >
+                  <SiGoogleclassroom className="text-xl font-bold" />
+                  <span className="ml-3 flex-1 whitespace-nowrap">
+                    Enrolled Classes
+                  </span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/dashboard/payment"
+                  className="flex items-center rounded-lg p-2 text-gray-600 hover:bg-gray-200 hover:text-primary"
+                >
+                  <BsCreditCard2Back className="text-xl font-bold" />
+                  <span className="ml-3 flex-1 whitespace-nowrap">Payment</span>
+                </NavLink>
+              </li>
+            </>
+          )}
+          {isInstructor && (
+            <>
+              <li>
+                <NavLink
+                  to="/dashboard/addAClass"
+                  className="flex items-center rounded-lg p-2 text-gray-600 hover:bg-gray-200 hover:text-primary"
+                >
+                  <AiOutlineVideoCameraAdd className="text-xl font-bold" />
+                  <span className="ml-3 flex-1 whitespace-nowrap">
+                    Add a class
+                  </span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/dashboard/myClass"
+                  className="flex items-center rounded-lg p-2 text-gray-600 hover:bg-gray-200 hover:text-primary"
+                >
+                  <BsFillGridFill className="text-xl font-bold" />
+                  <span className="ml-3 flex-1 whitespace-nowrap">
+                    My Classes
+                  </span>
+                </NavLink>
+              </li>
+            </>
+          )}
           {isAdmin && (
             <>
               <li>
@@ -56,62 +122,27 @@ const DashboardLayout = () => {
               </li>
             </>
           )}
-
-          <li>
-            <NavLink
-              to="/dashboard/myClass"
-              className="flex items-center rounded-lg p-2 text-gray-600 hover:bg-gray-200 hover:text-primary"
-            >
-              <BsFillGridFill className="text-xl font-bold" />
-              <span className="ml-3 flex-1 whitespace-nowrap">My Classes</span>
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink
-              to="/dashboard/addAClass"
-              className="flex items-center rounded-lg p-2 text-gray-600 hover:bg-gray-200 hover:text-primary"
-            >
-              <AiOutlineVideoCameraAdd className="text-xl font-bold" />
-              <span className="ml-3 flex-1 whitespace-nowrap">Add a class</span>
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink
-              to="/dashboard/selectedClasses"
-              className="flex items-center rounded-lg p-2 text-gray-600 hover:bg-gray-200 hover:text-primary"
-            >
-              <MdOutlineClass className="text-xl font-bold" />
-              <span className="ml-3 flex-1 whitespace-nowrap">
-                Selected Classes
-              </span>
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink
-              to="/dashboard/enrolledClasses"
-              className="flex items-center rounded-lg p-2 text-gray-600 hover:bg-gray-200 hover:text-primary"
-            >
-              <SiGoogleclassroom className="text-xl font-bold" />
-              <span className="ml-3 flex-1 whitespace-nowrap">
-                Enrolled Classes
-              </span>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/dashboard/payment"
-              className="flex items-center rounded-lg p-2 text-gray-600 hover:bg-gray-200 hover:text-primary"
-            >
-              <BsCreditCard2Back className="text-xl font-bold" />
-              <span className="ml-3 flex-1 whitespace-nowrap">Payment</span>
-            </NavLink>
-          </li>
         </ul>
         {/* these is general links for all  */}
         <ul className="mt-4 space-y-2 border-t border-gray-200 pt-4 font-medium dark:border-gray-700">
+          <li>
+            <NavLink
+              to="/"
+              className="flex items-center rounded-lg p-2 text-gray-600 hover:bg-gray-200 hover:text-primary"
+            >
+              <AiFillHome className="text-xl font-bold" />
+              <span className="ml-3 flex-1 whitespace-nowrap">Home</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/"
+              className="flex items-center rounded-lg p-2 text-gray-600 hover:bg-gray-200 hover:text-primary"
+            >
+              <AiFillHome className="text-xl font-bold" />
+              <span className="ml-3 flex-1 whitespace-nowrap">Home</span>
+            </NavLink>
+          </li>
           <li>
             <NavLink
               to="/"
